2bf71e0448c965eb134478dc973aa5505ea09dfb
einfalles <dn5jg@virginia.edu>
2016-04-04 23:23:22 -0700

    changes

--- app.js
+++ app.js
@@ -13,7 +13,46 @@ for (i=0;i<polys.length;i++) {
   }
 }
 $(document).ready(function(){
-  $('#bae').hover('mouseenter mouseleave', function (){
-
+  $('.polygon[data-id="8"]').mouseover(function(){
+    $('.trapezoid').css('border-bottom-width','40px');
   });
 });
+(function() {
+
+	function init() {
+		var speed = 350,
+			easing = mina.easeinout;
+    var market = $('.trap-content');
+    var rotation = 0;
+    var antirotation = -52;
+		[].slice.call ( document.querySelectorAll( '.svg-hover' ) ).forEach( function( el ) {
+      console.log(el.parentElement);
+			var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+				pathConfig = {
+					from : path.attr( 'd' ),
+					to : el.getAttribute( 'data-path-hover' )
+				};
+      el = el.parentElement.parentElement;
+			el.addEventListener( 'mouseenter', function() {
+				path.animate( { 'path' : pathConfig.to }, speed, easing );
+        market.css({
+            '-webkit-transform' :'translate(-20px,20px) '+'rotate('+ rotation +'deg)',
+                 '-moz-transform' : 'translate(-20px,20px) '+'rotate('+ rotation +'deg)',
+                 '-ms-transform' : 'translate(-20px,20px) '+'rotate('+ rotation +'deg)',
+                 'transform' : 'translate(-20px,20px) '+'rotate('+ rotation +'deg)'})
+			} );
+
+			el.addEventListener( 'mouseleave', function() {
+				path.animate( { 'path' : pathConfig.from }, speed, easing );
+        market.css({
+        '-webkit-transform' : 'translate(0%,0%) '+'rotate('+ antirotation +'deg)',
+                         '-moz-transform' : 'translate(0%,0%) '+'rotate('+ antirotation +'deg)',
+                         '-ms-transform' : 'translate(0%,0%) '+'rotate('+ antirotation +'deg)',
+                         'transform' : 'translate(0%,0%) '+'rotate('+ antirotation +'deg)'})
+			} );
+		} );
+	}
+
+	init();
+
+})();
