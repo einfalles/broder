1ef13d212e14331f5ece8e61b1f5d368b8e9a478
einfalles <dn5jg@virginia.edu>
2016-05-28 23:03:50 +0700

    yeah

--- new/js/main.js
+++ new/js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-  var fakeMargins = $(".header").height()*2
-  var mainWidth = $(window).width()-fakeMargins
-  $('.main').width(mainWidth)
+  var fakeMargins = $(".header").height()*2;
+  var mainWidth = $(window).width()-fakeMargins;
+  $('.main').width(mainWidth);
+  $('.header').width(mainWidth);
+  $('#menu-select').change(function(){
+    console.log(this.value);
+    if (this.value == 'eat-ost') {
+      var item = '#' + this.value;
+      $(item).show();
+      $('.content .section').not($(item)).hide();
+    }
+    if (this.value == 'eat-nord') {
+      var item = '#' + this.value;
+      $(item).show();
+      $('.content .section').not($(item)).hide();
+    }
+    if (this.value == 'eat-cafe') {
+      var item = '#' + this.value;
+      $(item).show();
+      $('.content .section').not($(item)).hide();
+    }
+  });
 });
