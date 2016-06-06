2b45f1dbba9ab83a66ce95e313368d545b3d1a0b
einfalles <dn5jg@virginia.edu>
2016-06-06 18:24:00 +0700

    yeah

---
+++ wah/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  var fakeMargins = $(".header").height()*2;
+  var mainWidth = $(window).width()-fakeMargins;
+  $('.main').width(mainWidth);
+  $('.header').width(mainWidth);
+  $('.footer').width(mainWidth);
+  $('#menu-select').change(function(){
+    console.log(this.value);
+    if (this.value == 'eat-ost') {
+      var item = '#' + this.value;
+      $(item).show();
+      $('.main .content').not($(item)).hide();
+    }
+    if (this.value == 'eat-nord') {
+      var item = '#' + this.value;
+      $(item).show();
+      $('.main .content').not($(item)).hide();
+    }
+    if (this.value == 'eat-cafe') {
+      var item = '#' + this.value;
+      $(item).show();
+      $('.main .content').not($(item)).hide();
+    }
+    if (this.value == 'eat-soder') {
+      var item = '#' + this.value;
+      $(item).show();
+      $('.main .content').not($(item)).hide();
+    }
+  });
+
+  var places = ['Hello','Howdy','Good Morning','Whats up?','Hi']
+  //  setInterval(hello, 600);
+  setInterval(function(){
+    var helloIndex = Math.floor((Math.random() * 5));
+    $('.hallo__wrapper h1').fadeOut(300,function(){
+      var $this = $(this);
+      $this.text(places[helloIndex]);
+      $this.fadeIn(500);
+    });
+  },6000);
+  pos_hallo();
+  function pos_hallo() {
+    var bottom = $("#cafe").height() + $("#cafe").offset()['top']-115;
+    var right = $("#cafe").offset()['left'] + $("#cafe").width()-115;
+    var pos = {'bottom': bottom, 'right': right};
+    $(".hallo").css('top', pos['bottom']);
+    $(".hallo").css('left', pos['right']);
+  }
+
+});
